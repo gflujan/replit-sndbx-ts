68419e3f1ff3a4a550be36584e9c64b6a6eb0631
Gabriel F. Lujan <gfl.wrk@gmail.com>
2022-04-25 22:30:12 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+console.log('==================================');
+console.log();
+/* Above is a space separator to give my reading some "breathing room" */
+
+console.log('sloane ❤️');
+
+/* Below is a space separator to give my reading some "breathing room" */
+console.log();
+console.log('==================================');
+
+// more 
